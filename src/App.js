620dcc9fffe3af66309fd5b7c513ea0143b93768
imjchiang <jchiang296@gmail.com>
2020-08-28 14:20:42 -0700

    [about] polish about page [app] add about route

--- src/App.js
+++ src/App.js
@@ -9,12 +9,16 @@ import
 } from "react-router-dom";
 
 import Home from "./routes/Home";
+import About from "./routes/About";
 
-function App() {
+function App() 
+{
+  const hobbies = ["playing piano and cello (classical)", "drinking boba", "eating sweets", "taking trips to national parks", "walking through museums", "reading outside on sunny days in the shade", "playing badminton", "board games", "video games (league, overwatch, apex, pubg, etc...)"]
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Home} />
+        <Route path="/about" render={() => <About hobbies={hobbies} />} />
       </div>
     </Router>
   );
--- src/routes/About.js
+++ src/routes/About.js
@@ -12,11 +12,11 @@ class About extends Component
             <div>
                 <h1>About Me!</h1>
                 <p>My name is Josh and I was born and raised in San Jose.</p>
-                <p>I attended UCLA to pursue a degree in Biochemistry as \n I thought that I would like to become a doctor</p>
-                <p>But as I pursued this dream, I realized that it conflicted \n with my goals of serving in my church and having time immediately after college to begin preparing to have time to raise a family</p>
-                <p>So I decided to drop the plans of med school and attended bootcamp to do software development as I found interest in it in my fourth year at UCLA</p>
+                <p>I attended UCLA to pursue a degree in Biochemistry as I thought that I would like to become a doctor</p>
+                <p>As I pursued my dream of becoming a doctor, I realized that it conflicted with my goals of serving in my church and having time immediately after college to begin preparing to have time to raise a family</p>
+                <p>So I decided to drop the plans of med school and attended bootcamp to do software development which I found interest in during my fourth year at UCLA in a C++ class.</p>
                 <br />
-                <p>Here are some things I enjoy doing: </p>
+                <h3>Here are some things I enjoy doing: </h3>
                 {allHobbies}
             </div>
         )
