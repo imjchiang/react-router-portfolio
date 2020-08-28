d814ed7a68fe7a5e1aecf42c373a4e2a53d8370a
imjchiang <jchiang296@gmail.com>
2020-08-28 14:02:11 -0700

    [app] create template [home] add component

---
+++ src/routes/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class Home extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h1>Welcome to Joshua Chiang's Portfolio</h1>
+            </div>
+        )
+    }
+}
